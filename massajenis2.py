7630a8d2352436c99a4f4faccf2a08bc0af8b9bb
dhitafebrianti <49123000+dhitafebrianti@users.noreply.github.com>
2019-10-16 19:01:39 +0800

    Create massajenis2.py

---
+++ massajenis2.py
@@ -0,0 +1,16 @@
+import mj
+
+def main():
+    m = 2
+    v = 6
+
+    p = mj.massajenis(m,v)
+
+    print("MASSA JENIS")
+    print("Massa\t: ", m)
+    print("Volume\t: ", v)
+    print("Massa Jenis\t= ", p)
+
+if __name__ == "__main__":
+    main()
+    
